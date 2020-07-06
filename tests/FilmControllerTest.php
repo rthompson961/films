a3791a89928da71e25df3a9a39acefdfd5b54c85
richard <richard@email.com>
2020-07-06 13:00:29 +0100

    added real functional tests

--- tests/FilmControllerTest.php
+++ tests/FilmControllerTest.php
@@ -6,11 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FilmControllerTest extends WebTestCase
 {
-    public function testSomething()
+    public function testIndex()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        $this->assertResponseIsSuccessful();
+        $this->assertEquals(2, $crawler->filter('h4')->count());
+    }
+
+    public function testShow()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/show/1');
+
+        $this->assertSelectorTextSame('p', 'There are 3 comments.');
+        $this->assertEquals(2, $crawler->filter('h4')->count());
+        $this->assertEquals('/show/1?offset=2', $crawler->filter('a')->attr('href'));
     }
 }
