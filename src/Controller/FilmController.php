22d9e0767bd6641f0f5891d478291910e9abe491
richard <richard@email.com>
2020-07-09 19:58:50 +0100

    added comment form

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Film;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,8 +25,31 @@ class FilmController extends AbstractController
     /**
      * @Route("/show/{slug}", name="film")
      */
-    public function show(Film $film, Request $request, CommentRepository $commentRepository)
+    public function show(Film $film, Request $request, CommentRepository $commentRepository, string $photoDir)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $comment->setFilm($film);
+            if ($photo = $form['photofile']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload photo
+                }
+                $comment->setPhoto($filename);
+            }
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('film', ['slug' => $film->getSlug()]);
+        }
+
         // offset must be an integer no less than zero
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($film, $offset);
@@ -32,7 +58,8 @@ class FilmController extends AbstractController
             'film' => $film,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/CommentFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+
+class CommentFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author', null, ['label' => 'Name'])
+            ->add('text')
+            ->add('email', EmailType::class)
+            ->add('photofile', FileType::class, [
+                'required'    => false,
+                'mapped'      => false,
+                'constraints' => [
+                    new Image(['maxSize' => '256k'])
+                ]
+            ])
+            ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
