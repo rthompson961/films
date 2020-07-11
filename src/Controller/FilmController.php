b94fc0d93e30b138054adc77bbc1658e5e1b8ad3
richard <richard@email.com>
2020-07-11 13:58:40 +0100

    added akismet spam checker

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -7,6 +7,7 @@ use App\Entity\Film;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\FilmRepository;
+use App\SpamChecker;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,8 +26,13 @@ class FilmController extends AbstractController
     /**
      * @Route("/show/{slug}", name="film")
      */
-    public function show(Film $film, Request $request, CommentRepository $commentRepository, string $photoDir)
-    {
+    public function show(
+        Film $film,
+        Request $request,
+        CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
+        string $photoDir
+    ) {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
         $form->handleRequest($request);
@@ -34,7 +40,10 @@ class FilmController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
 
             $comment->setFilm($film);
+
+            // if photo submitted
             if ($photo = $form['photofile']->getData()) {
+                // random file name
                 $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
                 try {
                     $photo->move($photoDir, $filename);
@@ -45,6 +54,17 @@ class FilmController extends AbstractController
             }
 
             $entityManager->persist($comment);
+
+            // spam check
+            $context = [
+                'user_ip'    => $request->getClientIp(),
+                'user_agent' => $request->headers->get('referer'),
+                'permalink'  => $request->getUri(),
+            ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Blatant spam detected');
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('film', ['slug' => $film->getSlug()]);
---
+++ src/SpamChecker.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App;
+
+use App\Entity\Comment;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class SpamChecker
+{
+    private $client;
+    private $endpoint;
+    private $publicDns;
+
+    public function __construct(HttpClientInterface $client, string $akismetKey, string $publicDns)
+    {
+        $this->client = $client;
+        $this->endpoint = sprintf('https://%s.rest.akismet.com/1.1/comment-check', $akismetKey);
+        $this->publicDns = $publicDns;
+    }
+
+    /**
+     * @return int Spam score: 0: not spam, 1: maybe spam, 2: blatant spam
+     * email akismet-guaranteed-spam@example.com always returns spam
+     *
+     * @throws \RuntimeException if the call did not work
+     */
+    public function getSpamScore(Comment $comment, array $context): int
+    {
+        $response = $this->client->request('POST', $this->endpoint, [
+            'body' => array_merge($context, [
+                'blog' => $this->publicDns,
+                'comment_type' => 'comment',
+                'comment_author' => $comment->getAuthor(),
+                'comment_author_email' => $comment->getEmail(),
+                'comment_content' => $comment->getText(),
+                'comment_date_gmt' => $comment->getCreatedAt()->format('c'),
+                'blog_lang' => 'en',
+                'blog_charset' => 'UTF-8',
+                'is_test' => true
+            ])
+        ]);
+
+        $headers = $response->getHeaders();
+        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+            return 2;
+        }
+
+        $content = $response->getContent();
+        if (isset($headers['x-akismet-debug-help'][0])) {
+            throw new \RuntimeException(sprint(
+                'Unable to check for spam: $s (%s).',
+                $content,
+                $headers['x-akismet-debug-help'][0]
+            ));
+
+            return 'true' === $content ? 1 : 0;
+        }
+
+        return 2;
+    }
+}
