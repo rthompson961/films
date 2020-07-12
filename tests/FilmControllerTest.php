ae757daadbdc0ed6357cb4ed018ec39757a0a764
richard <richard@email.com>
2020-07-12 14:28:09 +0100

    added comment form test

--- tests/FilmControllerTest.php
+++ tests/FilmControllerTest.php
@@ -19,7 +19,16 @@ class FilmControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/show/the-shining');
 
-        $this->assertSelectorTextSame('p', 'There are 3 comments.');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Sarah',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photofile]' => dirname(__DIR__) . '/public/uploads/' . '653babf4bbf5.jpg',
+        ]);
+
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorTextSame('p', 'There are 4 comments.');
         $this->assertEquals(2, $crawler->filter('h4')->count());
         $this->assertEquals('/show/the-shining?offset=2', $crawler->filter('a')->last()->attr('href'));
     }
