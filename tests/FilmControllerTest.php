898477fa55872d1f60e01b139182704d2d457b87
richard <richard@email.com>
2020-08-08 18:48:06 +0100

    added comment submission confirmation message assertion

--- tests/FilmControllerTest.php
+++ tests/FilmControllerTest.php
@@ -25,22 +25,22 @@ class FilmControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/show/the-shining');
 
         $client->submitForm('Submit', [
-            'comment_form[author]' => 'Sarah',
+            'comment_form[author]' => $author = 'Sarah',
             'comment_form[text]' => 'Some feedback from an automated functional test',
             'comment_form[email]' => $email = 'me@automat.ed',
             'comment_form[photofile]' => dirname(__DIR__) . '/public/uploads/653babf4bbf5.jpg',
         ]);
-
-        $this->assertResponseRedirects();
 
         // simulate comment validation
         $comment = self::$container->get(CommentRepository::class)->findOneByEmail($email);
         $comment->setState('published');
         self::$container->get(EntityManagerInterface::class)->flush();
 
+        $this->assertResponseRedirects();
         $client->followRedirect();
+        $this->assertSelectorTextSame('div.alert', 'Thank you, your comment will be posted after moderation.');
         $this->assertEquals(2, $crawler->filter('h4')->count());
-        $this->assertSelectorTextSame('h4', 'Sarah');
+        $this->assertSelectorTextSame('h4', $author);
         $this->assertSelectorTextSame('div.comment-count', 'There are 4 comment(s)');
         $this->assertEquals('/show/the-shining?offset=2', $crawler->filter('a.next')->attr('href'));
     }
